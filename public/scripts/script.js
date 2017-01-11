779549e01fecb29727b469b70440541db7e2c32a
Millie Walsh <millie@primeacademy.io>
2017-01-11 11:38:25 -0600

    add register route

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,3 +3,24 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController',['$scope', '$http', function($scope, $http) {
 
 }]);
+
+myApp.controller('registerController',['$scope', '$http', function($scope, $http) {
+  console.log('registerController');
+  $scope.register = function() {
+    console.log($scope.username, $scope.password);
+
+    var toSend = {
+      username: $scope.username,
+      password: $scope.password
+    };
+
+    $http({
+      method: 'POST',
+      url: '/register',
+      data: toSend
+    }).then(function(response) {
+      console.log('in client side success', response);
+    });
+
+  };
+}]);
--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var User = require('../models/user');
 
 router.get('/', function(req, res) {
   var regPath = path.join(__dirname, '../public/views/register.html');
   res.sendFile(regPath);
+});
+
+router.post('/', function(req, res) {
+  console.log('in post register req.body -> ', req.body);
+
+  var sentUser = req.body;
+  User.create(sentUser, function(err, response) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      res.status(201).send({message: "new user created!"});
+    }
+  });
 });
 
 module.exports = router;
