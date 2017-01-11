bb1048f79d282329f64735cc30fb1c40c4a7bfe1
Millie Walsh <millie@primeacademy.io>
2017-01-11 14:19:00 -0600

    add encryption

--- models/user.js
+++ models/user.js
@@ -1,9 +1,29 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
+
+var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true}
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this; // represent the user that we are save
+
+  if(!user.isModified('password')){
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 var User = mongoose.model('users', UserSchema);
